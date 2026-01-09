5fea40a9c3f37941d95dec92bf7665eca830c9cb
Matthew Wong <matthew@thewongcode.net>
2026-01-09 13:38:04 -0700

    Fix: removed unused import

--- src/Raw1001/main.ts
+++ src/Raw1001/main.ts
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 /* Copyright © 2025 Inkdex */
 
-import { ContentRating } from "@paperback/types";
 import { Liliana } from "../generic/Liliana";
 import pbconfig from "./pbconfig";
 
